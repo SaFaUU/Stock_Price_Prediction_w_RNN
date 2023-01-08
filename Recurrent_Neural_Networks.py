f79c5082bdadff970b3adc21e025d34ba5bc6589
Safa Asgar <thulk283@gmail.com>
2023-01-08 19:40:51 +0600

    Made Data Structure for timesteps for the training data

--- Recurrent_Neural_Networks.py
+++ Recurrent_Neural_Networks.py
@@ -14,6 +14,13 @@ from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range=(0, 1))
 training_set_scaled = sc.fit_transform(training_set)
 
+# Creating a Data Structure with 60 Timesteps and 1 Output
+X_train = []
+y_train = []
+for i in range(60, len(training_set_scaled)):
+    X_train.append(training_set_scaled[i-60:i,0])
+    y_train.append(training_set_scaled[i,0])
+X_train, y_train = np.array(X_train), np.array(y_train)
 # Building the RNN
 
 # Making Predictions and Visualising the results
