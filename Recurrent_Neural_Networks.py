1f0d7a3fdb220e9e88c720aa0c2c010489bddea0
Safa Asgar <thulk283@gmail.com>
2023-01-09 16:28:32 +0600

    Added the LSTM Layers

--- Recurrent_Neural_Networks.py
+++ Recurrent_Neural_Networks.py
@@ -21,6 +21,37 @@ for i in range(60, len(training_set_scaled)):
     X_train.append(training_set_scaled[i-60:i,0])
     y_train.append(training_set_scaled[i,0])
 X_train, y_train = np.array(X_train), np.array(y_train)
+
+# Reshaping
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
 # Building the RNN
+# Importing the libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+# Initializing the RNN
+regressor = Sequential()
+
+# Adding the first LSTM layer and some dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences=True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+# Adding the second LSTM layer and some dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+# Adding the third LSTM layer and some dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+# Adding the fourth LSTM layer and some dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences=False))
+regressor.add(Dropout(0.2))
+
+# Adding the output layer
+regressor.add(Dense(units=1))
 
 # Making Predictions and Visualising the results
